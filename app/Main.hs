1df41099377c4f879a42a48e3b9c99dbdfc4b18f
Sjur Millidahl <sjur.millidahl@statnett.no>
2026-01-05 11:41:58 +0100

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,6 @@ import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as TIO
 import System.Exit (exitSuccess)
-import System.IO (stdin)
 import Text.Read qualified as Text
 
 storage :: FilePath
@@ -18,11 +17,11 @@ data Exit = Exit
 main :: IO ()
 main =
   forever $ do
-    putStrLn "TODO"
-    putStrLn "----"
     todos <- Text.lines <$> TIO.readFile storage
+    TIO.putStrLn "TODO"
+    TIO.putStrLn "----"
     traverse_ (print @(Int, Text)) ([0 ..] `zip` todos)
-    cmd <- TIO.hGetLine stdin
+    cmd <- TIO.getLine
     case doInput todos cmd of
       Left Exit -> exitSuccess
       Right newTodos -> TIO.writeFile storage (Text.unlines newTodos)
